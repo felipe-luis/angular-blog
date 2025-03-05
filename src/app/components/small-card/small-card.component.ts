a9a576ddca83256a0649693349b4cb2728b2265e
felipe-luis <felipellopes12@hotmail.com>
2025-03-05 11:07:46 -0300

    components createds with styles

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css',
+  
+})
+export class SmallCardComponent {
+
+}
