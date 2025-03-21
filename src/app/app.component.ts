cf11da7a3a34f64ec028ca5ff0e5b60b5d99d2f2
felipe-luis <felipellopes12@hotmail.com>
2025-03-21 14:03:38 -0300

    Adiciona imagens ao projeto, melhora a barra de menu e refatora páginas de home e content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
-import { HomeComponent } from "./pages/home/home.component";
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BigCardComponent, MenuTitleComponent, HomeComponent, MenuBarComponent],
+  imports: [RouterOutlet, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,12 +1,24 @@
 export const dataFake = [
     {
         "id": "1",
-        "title": "Novo contratado",
-        "description": "Avanade contrata Felipe como novo estagiário",
-        "photoCover" : "https://vagas.byintera.com/wp-content/uploads/2021/05/Previa-Preview-Starbucks-Capa-da-Landing-Page.png"
+        "title": "O principal especialista Microsoft do mundo",
+        "description": "Quando os resultados são importantes, as pessoas falam com a Avanade. Há mais de 25 anos que ajudamos clientes de todos os setores a atingir o seu potencial máximo, desbloqueando o poder da Microsoft.",
+        "photoCover" : "assets/imgs/avanade-logo-principal.jpg"
     },
     {
         "id": "2",
+        "title": "Os nossos serviços",
+        "description": "Trabalhamos consigo para encontrar a combinação certa de aconselhamento, experiência e tecnologia Microsoft para criar soluções tão práticas quanto poderosas.",
+        "photoCover" : "assets/imgs/our-services.jpg"
+    },
+    {
+        "id": "3",
+        "title": "A nossa experiência na indústria",
+        "description": "A Avanade combina conhecimentos tecnológicos com décadas de experiência em todos os setores. Descubra como podemos desbloquear as maiores oportunidades para a sua organização.",
+        "photoCover" : "assets/imgs/industry.jpg"
+    },
+    {
+        "id": "4",
         "title": "Melhor empresa!",
         "description": "Avanade é considerada a melhor empresa para se estagiar",
         "photoCover" : "https://media.licdn.com/dms/image/v2/D4D22AQGrBwPpxq72zQ/feedshare-shrink_800/B4DZPUsHXtGgAk-/0/1734440155435?e=2147483647&v=beta&t=T9AB2xkqXupJIKjZG5oYYbkqGe13q7AjJTxgk9wgdn0"
